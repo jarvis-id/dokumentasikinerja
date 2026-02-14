57510492002f40889d16b6b8b2beefed13f948af
Just A Rather Very Intelligent System <50509625+jarvis-id@users.noreply.github.com>
2026-02-14 10:36:09 +0800

    Add service worker for caching and fetch handling

---
+++ songkang/sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'absensi-pwa-cache-v1';
+const urlsToCache = [
+  '/',
+  'https://pdamone.amertatoraya.com'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
