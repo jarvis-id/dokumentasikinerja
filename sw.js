4f1e49b0e18aacfa28721fa8168db8cd7b85b609
Just A Rather Very Intelligent System <50509625+jarvis-id@users.noreply.github.com>
2026-04-23 03:17:15 +0000

    Menghapus semua file dan folder

--- sw.js
+++
@@ -1,5 +0,0 @@
-self.addEventListener('fetch', (event) => {
-    // Service worker minimal agar syarat PWA terpenuhi. 
-    // Strategi caching ringan saat fetch dilakukan otomatis di sini
-    event.respondWith(fetch(event.request));
-});
